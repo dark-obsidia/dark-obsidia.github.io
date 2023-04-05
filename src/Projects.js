7be7f3d8f49b3c98c3d23fccacb952d5c38edf92
Abel Lu <alu4@ncsu.edu>
2023-04-05 01:12:16 -0400

    added hyperlinked images for projects. need to add descriptions

--- src/Projects.js
+++ src/Projects.js
@@ -1,26 +1,34 @@
-import "./styles/index.css"
+import "./styles/index.css";
+import discord from "./images/discord.png";
+import osu from "./images/osu.png";
+import genshin from "./images/genshin.png";
 
 const Projects = () => {
     return (  
         <div className="Projects" id="Projects">
             <h2>Projects</h2>
-            <br></br>
-            <p> Hey there! I'm Abel Lu, a sophomore Computer Science student at North Carolina State University.
-                I'm currently enrolled in Software Development Fundamentals, a Java class where I've been learning
-                about software development best practices, unit testing, and finite state machines. I'm also taking
-                an introductory Data Science course where I've been working with RStudio and Python. Aside from my
-                coursework, I'm also working on a few personal projects, including a Genshin Impact-themed Discord bot
-                made using discord.py, as well as the digital portfolio you're reading right now, made with React.js.</p>
-            <br></br>
-            <p> Aside from being a Computer Science student, I'm also an avid music student! Here at NC State, I'm a member
-                of the Marching Band, where I play the snare drum, the Wind Ensemble, where I play percussion, and the
-                Beginning Band, where I'm learning how to play the flute. I'm also a big fan of college and professional
-                basketball. My favorite college team is the NC State Wolfpack, and my favorite NBA team is the Los Angeles
-                Lakers. In addition to just watching basketball, I also like to play, and I get the privilege of playing
-                with the best teammates I could ever ask for as a part of NC State's intramural basketball league.</p>
-            <br></br>
-            <p> Now that you know all about me, I'd love to get to know you as well! If you'd like to chat, feel free to
-                connect with me on LinkedIn or through my "Contact Me" page. I'll be more than happy to get in touch with you!</p>
+            <br/>
+            <div className="projects-container">
+                <div className="project">
+                    <a href="https://github.com/azn-abel?tab=repositories&language=python" target={'_blank'} rel={'noreferrer'}>
+                        <img alt="Discord Logo" src={discord}/>
+                    </a>
+                    <b>Discord Bots</b>
+                </div>
+                <div className="project">
+                    <a href="https://github.com/azn-abel/OsuBot" target={'_blank'} rel={'noreferrer'}>
+                        <img alt="osu! logo" src={osu}/>
+                    </a>
+                    <b>osu! API</b>
+                </div>
+                <div className="project">
+                    <a href="https://github.com/azn-abel/GenshinDiscordBot" target={'_blank'} rel={'noreferrer'}>
+                        <img alt="Genshin Impact Logo" src={genshin}/>
+                    </a>
+                    <b>Genshin Impact</b>
+                </div>
+            </div>
+            <br />
             <br></br>
             <iframe src="https://azn-abel.github.io/azn-abel/LuAbelResume.pdf" type="application/pdf" frameborder="0" title="Resume"></iframe>
                 
