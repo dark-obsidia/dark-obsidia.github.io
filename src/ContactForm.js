fd16114f4e09e2fa8e6f6c4e626ade7ea7acecea
Abel Lu <alu4@ncsu.edu>
2023-04-04 22:02:29 -0400

    added working emailer

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,50 +1,39 @@
 import "./styles/index.css";
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import emailjs from "@emailjs/browser";
 
 function ContactForm() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [subject, setSubject] = useState('');
     const [message, setMessage] = useState('');
-    const [status, setStatus] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const formData = new FormData();
-        formData.append('name', name);
-        formData.append('email', email);
-        formData.append('subject', subject);
-        formData.append('message', message);
-        
-        fetch('./send_email.php', {
-            method: 'POST',
-            body: formData
-        })
-        .then(response => response.text())
-        .then(data => {
-            setStatus(data);
-            setName('');
-            setEmail('');
-            setSubject('');
-            setMessage('');
-        })
-        .catch(error => console.log(error));
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_l49rbwz', 'template_oklhrkg', form.current, 'TalPMRUnzL8eHLRR5')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        e.target.reset()
     };
 
     return ( 
         <div className="ContactForm" id="ContactForm">
-            <form onSubmit={handleSubmit}>
-                <h1>Contact Us</h1>
-                {status && <p>{status}</p>}
+            <form ref={form} onSubmit={sendEmail}>
                 <label htmlFor="name">Name:</label>
-                <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} required />
+                <input type="text" id="name" name="name" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} required />
                 <label htmlFor="email">Email:</label>
-                <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                <input type="email" id="email" name="email" placeholder="Email Address" value={email} onChange={(e) => setEmail(e.target.value)} required />
                 <label htmlFor="subject">Subject:</label>
-                <input type="text" id="subject" value={subject} onChange={(e) => setSubject(e.target.value)} required />
+                <input type="text" id="subject" name="subject" placeholder="Subject" value={subject} onChange={(e) => setSubject(e.target.value)} required />
                 <label htmlFor="message">Message:</label>
-                <textarea id="message" value={message} onChange={(e) => setMessage(e.target.value)} required></textarea>
-                <button type="submit">Send</button>
+                <textarea id="message" name="message" placeholder="Your message" value={message} onChange={(e) => setMessage(e.target.value)} required/>
+                <input type="submit" className="btn btn-info" value="Send Message"/>
             </form>
         </div>
      );
