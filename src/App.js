d928ffe450a241be19b0cadb997a5c7fc502a5af
Abel Lu <alu4@ncsu.edu>
2023-04-11 14:00:30 -0400

    fixed image background loading to be smoother

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <h1 style={
+      <h1 className="fade-in" style={
         {
           backgroundImage: "linear-gradient(rgba(0,0,0,0.75), rgba(0,0,0,0.75)), url(" + Background + ")",
         }
