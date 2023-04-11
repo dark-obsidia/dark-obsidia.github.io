99b2a8bcc0bab64e37b24c55320dec562712ba4a
Abel Lu <alu4@ncsu.edu>
2023-04-11 11:57:44 -0400

    left aligned text, changef icons

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,8 @@ const Home = () => {
 
     return (
         <div className="Home" id="AboutMe">
+            <br /><br />
+            <h2>About Me</h2>
             <div className="about-me-container">
             <div className="about-me-box">
                 <br />
@@ -12,9 +14,7 @@ const Home = () => {
             </div>
 
             <div className="about-me-box">
-                <br />
                 <h3 className="Hello">Hi, I'm Abel Lu 👋</h3>
-                
                 <br/>
                 <p> Hey there! My name is Abel Lu, and I'm a first-year sophomore Computer Science student at North Carolina State University.
                     I'm currently enrolled in Software Development Fundamentals, a Java class where I've been learning
@@ -24,13 +24,13 @@ const Home = () => {
                     coursework, I'm also working on a few personal projects, including a Genshin Impact-themed Discord bot
                     being made using discord.py, as well as the digital portfolio you're reading right now, which has been
                     created with React.js, HTML, and CSS.</p>
-                <br></br>
+                {/* <br></br>
                 <p> Aside from being a Computer Science major, I'm also an avid music student! Here at NC State, I'm a member
                     of the Marching Band, where I play the snare drum, the Wind Ensemble, where I play percussion, and the
                     Beginning Band, where I'm learning how to play the flute. I'm also a big fan of college and professional
                     basketball. My favorite college team is the NC State Wolfpack, and my favorite NBA team is the Los Angeles
                     Lakers. In addition to just watching basketball, I also like to play! As a member of NC State's intramural
-                    basketball league, I get the privilege of playing in recreational matches with some of the best teammates in the world.</p>
+                    basketball league, I get the privilege of playing in recreational matches with some of the best teammates in the world.</p> */}
                 <br></br>
                 <p> Now that you know all about me, I'd love to get to know you as well! If you'd like to chat, feel free to
                     connect with me on LinkedIn or through my "Contact Me" page. I'll be more than happy to get in touch with you!</p>
--- src/Resume.js
+++ src/Resume.js
@@ -5,7 +5,7 @@ const Resume = () => {
         <div className="Resume" id="Resume">
             <br />
             <h2>Resume</h2>
-            <br></br>
+            <br/><br />
             <iframe src="https://azn-abel.github.io/azn-abel/LuAbelResume.pdf" type="application/pdf" frameborder="0" title="Resume"></iframe>    
         </div>
     );
